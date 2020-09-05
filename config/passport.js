f53a922590cf49faf326233eaa8e8ae4b0983712
Taylor Chen <s68175@gmail.com>
2020-09-06 02:37:01 +0800

    Add user authentication and message flash

--- config/passport.js
+++ config/passport.js
@@ -9,15 +9,15 @@ module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
 
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+  passport.use(new LocalStrategy({ passReqToCallback: true, usernameField: 'email' }, (req, email, password, done) => {
     User.findOne({ where: { email } })
       .then(user => {
         if (!user) {
-          return done(null, false, { message: 'That email is not registered!' })
+          return done(null, false, req.flash('error_msg', 'That email is not registered!'))
         }
         return bcrypt.compare(password, user.password).then(isMatch => {
           if (!isMatch) {
-            return done(null, false, { message: 'Email or Password incorrect.' })
+            return done(null, false, req.flash('error_msg', 'Email or Password incorrect.'))
           }
           return done(null, user)
         })
